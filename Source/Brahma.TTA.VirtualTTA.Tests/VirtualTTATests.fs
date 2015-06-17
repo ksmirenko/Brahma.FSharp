97424c30ba368c60a2f84748f7fc75d9300d3c15
gsv <rsdpisuy@gmail.com>
2015-06-17 10:32:13 +0300

    Simple virtual TTA with basic tests.

---
+++ Source/Brahma.TTA.VirtualTTA.Tests/VirtualTTATests.fs
@@ -0,0 +1,39 @@
+﻿module Brahma.TTA.VirtualTTA.Tests
+
+open NUnit.Framework
+
+[<Test>]
+let SimpleAdd () =
+    // 2 + 3
+    let board = new Board[|(fun () -> new Cell([|0;0|], 1, fun a -> a.[0] + a.[1])),2|]
+    board.Step [|Mvc (2,(0<ln>,0<col>,0<port>))|]
+    board.Step [|Mvc (3,(0<ln>,0<col>,1<port>))|]
+    let r = board.GetResult 0<ln> 0<col>
+    Assert.AreEqual(5,r)
+
+[<Test>]
+let Add () =
+    // 2 + 3 + 4 + 5
+    let board = new Board[|(fun () -> new Cell([|0;0|], 1, fun a -> a.[0] + a.[1])),2|]
+    board.Step [|Mvc (2,(0<ln>,0<col>,0<port>)); Mvc (4,(0<ln>,1<col>,0<port>))|]
+    board.Step [|Mvc (3,(0<ln>,0<col>,1<port>)); Mvc (5,(0<ln>,1<col>,1<port>))|]
+    board.Step [|Mov ((0<ln>,1<col>),(0<ln>,0<col>,0<port>))|]
+    board.Step [|Mov ((0<ln>,0<col>),(0<ln>,0<col>,1<port>))|]
+    let r = board.GetResult 0<ln> 0<col>
+    Assert.AreEqual(14,r)
+
+[<Test>]
+let AddMult () =
+    // 2 + 3 + 4 * 5
+    let board = 
+        new Board
+            [|
+                (fun () -> new Cell([|0;0|], 1, fun a -> a.[0] + a.[1])),2
+                (fun () -> new Cell([|0;0|], 1, fun a -> a.[0] * a.[1])),2
+            |]
+    board.Step [|Mvc (2,(0<ln>,0<col>,0<port>)); Mvc (4,(1<ln>,0<col>,0<port>))|]
+    board.Step [|Mvc (3,(0<ln>,0<col>,1<port>)); Mvc (5,(1<ln>,0<col>,1<port>))|]
+    board.Step [|Mov ((1<ln>,0<col>),(0<ln>,0<col>,0<port>))|]
+    board.Step [|Mov ((0<ln>,0<col>),(0<ln>,0<col>,1<port>))|]
+    let r = board.GetResult 0<ln> 0<col>
+    Assert.AreEqual(25,r)
---
+++ Source/Brahma.TTA.VirtualTTA/VirtualTTA.fs
@@ -0,0 +1,27 @@
+﻿namespace Brahma.TTA.VirtualTTA
+
+[<Measure>] type ln
+[<Measure>] type col
+[<Measure>] type port
+ 
+type Asm =
+    | Mov of (int<ln>*int<col>)*(int<ln>*int<col>*int<port>)
+    | Mvc of int*(int<ln>*int<col>*int<port>)
+
+type Cell(ports:array<int>,triggerPortId,action) =
+    let result = ref 0
+    member this.Set v i = 
+        ports.[i] <- v
+        if i = triggerPortId
+        then result := action ports
+    member this.GetResult () = !result
+
+type Board (rowDescriptions:array<(unit -> Cell)*int>) =
+    let board = Array.init  rowDescriptions.Length (fun i -> Array.init (snd rowDescriptions.[i]) (fun j -> (fst rowDescriptions.[i])()))
+    member this.Step (instruction:array<Asm>) =
+        instruction
+        |> Array.iter
+            (function | Mov ((lf,cf),(lt,ct,pt)) -> board.[int lt].[int ct].Set (board.[int lf].[int cf].GetResult()) (int pt)
+                      | Mvc (v,(lt,ct,pt)) -> board.[int lt].[int ct].Set v (int pt))
+
+    member this.GetResult (l:int<ln>) (c:int<col>) = board.[int l].[int c].GetResult()
