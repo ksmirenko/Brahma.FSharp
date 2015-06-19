b3a82221448b25ea34ed9cf0da6a120eedfcc399
Ekaterina Verbitskaia <kajigor@gmail.com>
2015-06-19 17:23:36 +0300

    VSFG in process

---
+++ Source/Brahma.TTA.VSFG.Test/VSFGTests.fs
@@ -0,0 +1,61 @@
+﻿module Brahma.TTA.VSFG.Tests
+
+open Brahma.TTA.VSFG
+open NUnit.Framework
+
+let calculate (node : UnaryNode) = 
+    (unbox node.Operation) node.InPorts.[0]
+
+[<Test>]
+let SmallGraph () =
+    let xOut = new OutPort()
+    let x = new InitialNode(xOut)
+
+    let yOut = new OutPort()
+    let y = new InitialNode(yOut)
+
+    let plusLeft = new InPort(xOut, x :> Node)
+    let plusRight = new InPort(yOut)
+    let plusOut = new OutPort()
+    let plus = new AddNode(plusLeft, plusRight, plusOut)
+
+
+    let lessLeft = new InPort(xOut)
+    let lessRight = new InPort(yOut)
+    let lessOut = new OutPort()
+    let less = new LtNode(lessLeft, lessRight, lessOut)
+
+    let incIn = new InPort(xOut)
+    let incOut = new OutPort()
+    let inc = new IncNode(incIn, incOut)
+
+    let decIn = new InPort(yOut)
+    let decOut = new OutPort()
+    let dec = new DecNode(decIn, decOut)
+
+    let xIn = new InPort(incOut)
+    x.InPorts.Add(xIn)
+    
+    let yIn = new InPort(decOut)
+    y.InPorts.Add(yIn)
+
+    let fOut = new OutPort()
+    let f = new BinaryNode(xIn, yIn, fOut, null)
+    
+    let multiplexorOut = new OutPort()
+    let predicate = new InPort(lessOut)
+    let _true = new InPort(fOut)
+    let _false = new InPort(plusOut)
+    let multiplexor = new MultiplexorNode(predicate, _true, _false, multiplexorOut)
+
+    let terminalIn = new InPort(multiplexorOut)
+    let terminal = new TerminalNode(terminalIn)
+
+    Assert.NotNull(x.PrevNodes)
+    Assert.AreEqual(1, x.PrevNodes.Length)
+
+    Assert.NotNull(y.PrevNodes)
+    Assert.AreEqual(1, y.PrevNodes.Length)
+    
+
+
---
+++ Source/Brahma.TTA.VSFG/VSFG.fs
@@ -0,0 +1,72 @@
+﻿namespace Brahma.TTA.VSFG
+
+type INode = interface end
+
+
+type Port () = 
+    interface INode
+
+type InPort (inputs : ResizeArray<OutPort>) =
+    inherit Port ()
+    member this.Inputs = inputs
+    member this.ConnectWith p = this.Inputs.Add p
+    new () = InPort (new ResizeArray<_>())
+    new (x : OutPort) = InPort (new ResizeArray<_>([|x|]))
+
+and OutPort (targets : ResizeArray<InPort>) =
+    inherit Port ()
+    member this.Targets = targets
+    member this.ConnectWith p = this.Targets.Add p
+    new () = OutPort (new ResizeArray<_>())
+    new (x : InPort) = OutPort (new ResizeArray<_>([|x|]))
+
+
+type Node (inPorts : ResizeArray<InPort>, outPorts : ResizeArray<OutPort>, operation) = 
+    interface INode
+    member this.InPorts = inPorts
+    member this.OutPorts = outPorts
+    member this.Operation = operation
+
+
+type UnaryNode (inPort : InPort, outPort : OutPort, operation) =
+    inherit Node (new ResizeArray<_>([|inPort|]), new ResizeArray<_>([|outPort|]), operation)
+
+type NegativeNode (inPort : InPort, outPort : OutPort) =
+    inherit UnaryNode (inPort, outPort, box (~-))
+
+type IncNode (inPort : InPort, outPort : OutPort) =
+    inherit UnaryNode (inPort, outPort, box (fun x -> x + 1))
+
+type DecNode (inPort : InPort, outPort : OutPort) =
+    inherit UnaryNode (inPort, outPort, box (fun x -> x - 1))
+
+
+type BinaryNode (left : InPort, right : InPort, outPort : OutPort, operation) =
+    inherit Node (new ResizeArray<_>([|left; right|]), new ResizeArray<_>([|outPort|]), operation)
+
+type AddNode (left : InPort, right : InPort, outPort : OutPort) =
+    inherit BinaryNode (left, right, outPort, box (fun x y -> x + y))
+
+type SubNode (left : InPort, right : InPort, outPort : OutPort) =
+    inherit BinaryNode (left, right, outPort, box (fun x y -> x - y))
+
+type MulNode (left : InPort, right : InPort, outPort : OutPort) =
+    inherit BinaryNode (left, right, outPort, box (fun x y -> x * y))
+
+type DivNode (left : InPort, right : InPort, outPort : OutPort) =
+    inherit BinaryNode (left, right, outPort, box (fun x y -> x / y))
+
+type EqNode (left : InPort, right : InPort, outPort : OutPort) =
+    inherit BinaryNode (left, right, outPort, box (fun x y -> x = y))
+
+type GtNode (left : InPort, right : InPort, outPort : OutPort) =
+    inherit BinaryNode (left, right, outPort, box (fun x y -> x > y))
+
+type LtNode (left : InPort, right : InPort, outPort : OutPort) =
+    inherit BinaryNode (left, right, outPort, box (fun x y -> x < y))
+
+type GeqNode (left : InPort, right : InPort, outPort : OutPort) =
+    inherit BinaryNode (left, right, outPort, box (fun x y -> x >= y))
+
+type LeqNode (left : InPort, right : InPort, outPort : OutPort) =
+    inherit BinaryNode (left, right, outPort, box (fun x y -> x <= y))
