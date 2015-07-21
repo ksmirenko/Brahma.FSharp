9504fcd186b9e8beb250635a3e6492df037ba51c
Ivan <ivan.afonichkin@gmail.com>
2015-07-21 19:19:51 +0300

    Added VirtualTTA and tests for it

--- Source/Brahma.TTA.VirtualTTA.Tests/VirtualTTATests.fs
+++ Source/Brahma.TTA.VirtualTTA.Tests/VirtualTTATests.fs
@@ -3,6 +3,59 @@
 open NUnit.Framework
 
 [<Test>]
+let oneFU () = 
+    let FU = ADD(0<inPort>, 0<inPort>, true)
+    let TTA = new TTA([| (FU, 1) |], 1)
+    
+    (* Information about function unit doesn't matter, matters just type of FU (ex: ADD, SUB, etc..) 
+     * So, we can write TTA.IsFreeFU(ADD(13<inPort>, 16<inPort>, false))
+     *)
+    let isFreeFU = TTA.IsFreeFU(ADD(0<inPort>, 0<inPort>, true))
+    Assert.IsTrue(isFreeFU)
+
+    let ind = TTA.GetFreeFU(ADD(0<inPort>, 0<inPort>, true))
+    Assert.IsTrue((ind = (0,0)))
+
+    TTA.SetFUAsNonEmpty(ind)
+    let isFreeFU = TTA.IsFreeFU(ADD(0<inPort>, 0<inPort>, true))
+    Assert.IsFalse(isFreeFU)
+
+    TTA.SetFUAsEmpty(ind)
+    let isNonFreeFU = TTA.IsFreeFU(ADD(0<inPort>, 0<inPort>, true))
+    Assert.IsTrue(isNonFreeFU)
+
+[<Test>]
+let twoSameFU() = 
+    let FU = ADD(0<inPort>, 0<inPort>, true)
+    let TTA = new TTA([| (FU, 2) |], 1)
+
+    (* Check first FU *)
+    let isFreeFU0 = TTA.IsFreeFU(ADD(0<inPort>, 0<inPort>, true))
+    Assert.IsTrue(isFreeFU0)
+    let ind0 = TTA.GetFreeFU(ADD(0<inPort>, 0<inPort>, true))
+    TTA.SetFUAsNonEmpty(ind0)
+
+    (* Check second FU *)
+    let isFreeFU1 = TTA.IsFreeFU(ADD(0<inPort>, 0<inPort>, true))
+    Assert.IsTrue(isFreeFU1)
+    let ind1 = TTA.GetFreeFU(ADD(0<inPort>, 0<inPort>, true))
+    TTA.SetFUAsNonEmpty(ind1)
+
+    (* All FUs are not free *)
+    let isAnybodyFree = TTA.IsFreeFU(ADD(0<inPort>, 0<inPort>, true))
+    Assert.IsFalse(isAnybodyFree)
+
+    (* Free one FU *)
+    TTA.SetFUAsEmpty(ind0)
+
+    (* Check for free *)
+    Assert.IsTrue(TTA.IsFreeFU(ADD(0<inPort>, 0<inPort>, true)))
+
+
+
+
+(*
+[<Test>]
 let SimpleAdd () =
     // 2 + 3
     let board = new Board [|(fun () -> new Cell([|0; 0|], 1, fun a -> a.[0] + a.[1])), 2|]
@@ -37,3 +90,5 @@ let AddMult () =
     board.Step [|Mov ((0<ln>, 0<col>), (0<ln>, 0<col>, 1<port>))|]
     let r = board.GetResult 0<ln> 0<col>
     Assert.AreEqual(25, r)
+
+*)
