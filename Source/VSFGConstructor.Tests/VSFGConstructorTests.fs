40c926942d25fbe77c2963e0a3eabdb76552d4c8
Mikhail-M <mihail-maryfich@yandex.ru>
2015-07-27 04:12:03 +0300

    working without recLet

--- Source/VSFGConstructor.Tests/VSFGConstructorTests.fs
+++ Source/VSFGConstructor.Tests/VSFGConstructorTests.fs
@@ -2,8 +2,21 @@
 
 open Brahma.TTA.VSFG
 open Brahma.TTA.VSFGConstructorHelper
+open Brahma.TTA.VSFGConstructor
+open System.Collections.Generic
 
 open NUnit.Framework
+open Microsoft.FSharp.Compiler.SourceCodeServices
+
+let checkNeighbours (node : Node) prevCount nextCount =
+    let prevNodes = node.GetPrevNodes()
+    let nextNodes = node.GetNextNodes()
+
+    Assert.NotNull(prevNodes)
+    Assert.AreEqual(prevCount, prevNodes.Count)
+
+    Assert.NotNull(nextNodes)
+    Assert.AreEqual(nextCount, nextNodes.Count)
 
 [<Test>]
 let VSFGConstructorHelper() = 
@@ -29,9 +42,28 @@ let VSFGConstructorHelper() =
            x + y
     let sum x + y = x + y
     "
-
     let t = new TypedTreeGetter(input2) 
     Assert.AreEqual (t.CountOfDecl, 2)
     Assert.AreEqual ((0 |> t.getFSharpArgs).Length, 3)
 
-   
+[<Test>]
+let WithoutRec() = 
+    
+    let t = VSFGConstructor("
+
+   let main (x:int) :int =
+    let g x y = x + y 
+    let f (x:int) (y:int) :int =
+        let e x y = x + y
+        if x > y then
+           g (x) (x+y)
+        else 
+            x - y
+    f 2 x
+    "
+        )
+    let vsfg = t.getVSFG (t.Helper.getFSharpExpr 0)
+    Assert.AreEqual (vsfg.InitialNodes.Length, 1)
+    
+    checkNeighbours vsfg.TerminalNodes.[0] 1 0
+
