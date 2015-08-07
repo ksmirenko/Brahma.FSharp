bacc356a8cf404ad75e2d981c4307b40af99b967
Ivan <ivan.afonichkin@gmail.com>
2015-08-07 18:58:14 +0300

    Added tests

--- Source/Brahma.TTA.VSFGCompiler.Tests/VSFGCompilerTests.fs
+++ Source/Brahma.TTA.VSFGCompiler.Tests/VSFGCompilerTests.fs
@@ -6,75 +6,20 @@ open Brahma.TTA.VSFGCompiler
 open Brahma.TTA.VSFGConstructor
 open NUnit.Framework
 
-[<Test>]
-let VSFGCompilerWithoutMUXandFunctions () = 
-    let x = new InitialNode()
-    let y = new InitialNode()
-    
-    x.ResultAddr <- (1<ln>, 0<col>)
-    y.ResultAddr <- (1<ln>, 1<col>)
-    x.Status <- Ready (* Can we do it automatically? *)
-
-    let terminal = new TerminalNode()
-
-    terminal.ResultAddr <- (1<ln>, 2<col>)
-
-    let plus1 = new AddNode()
-    let plus2 = new AddNode()
-    let plus3 = new AddNode()
-    let plus4 = new AddNode()
-    let plus5 = new AddNode()
-    let plus6 = new AddNode() 
-
-    let vsfg = new VSFG ([|x; y|], [|terminal|], [||])
-
-    VSFG.AddVerticesAndEdges 
-        [|
-            x :> Node, 0, plus1 :> Node, 0;
-            y :> Node, 0, plus1 :> Node, 1;
-            x :> Node, 0, plus2 :> Node, 0;
-            y :> Node, 0, plus2 :> Node, 1;
-            x :> Node, 0, plus3 :> Node, 0;
-            y :> Node, 0, plus3 :> Node, 1;
-
-            plus1 :> Node, 0, plus4 :> Node, 0;
-
-            plus2 :> Node, 0, plus4 :> Node, 1
-            plus2 :> Node, 0, plus5 :> Node, 0;
-
-            plus3 :> Node, 0, plus5 :> Node, 1;
-
-            plus4 :> Node, 0, plus6 :> Node, 0;
-            plus5 :> Node, 0, plus6 :> Node, 1;
-
-            plus6 :> Node, 0, terminal :> Node, 0;
-        |]
-
-    let FU1 = ADD("in1", "in2t", "out1", true)
-    let FU2 = REGISTER("0", true)
-    let TTA = new TTA([| (FU1, 5); (FU2, 4) |], 1)
-
-    let compiler = new VSFGCompiler(vsfg, TTA)
-
-    let code = compiler.Compile()
-
-    ()
-
-    (*
-    let file = new System.IO.StreamWriter(@"C:\Users\User\Documents\Workspace\test.txt")
+let printCodeInFile(tta : TTA, code : ResizeArray<AsmType array>, filePath : string) = 
+    let file = new System.IO.StreamWriter(filePath)
 
     code.ForEach( 
         fun x -> 
         ( 
             //printf "("
-            Array.iter( fun y -> file.Write(Asm.toString(y, TTA)); file.Write("; ")) x
+            Array.iter( fun y -> file.Write(Asm.toString(y, tta)); file.Write("; ")) x
             file.WriteLine()
             //printfn ")"
         ) 
     )
 
     file.Close()
-    *)
 
 [<Test>]
 let SimpleCompilingFromFSharpToAsm() = 
@@ -97,27 +42,17 @@ let SimpleCompilingFromFSharpToAsm() =
     let FU2 = REGISTER("0", true)
     let TTA = new TTA([| (FU1, 5); (FU2, 4) |], 1)
 
+    TTA.SetFUAsNonFree(inits.[0].ResultAddr)
+    TTA.SetFUAsNonFree(inits.[1].ResultAddr)
+    TTA.SetFUAsNonFree(terminals.[0].ResultAddr)
+
     let compiler = new VSFGCompiler(vsfg, TTA)
 
     let code = compiler.Compile()
 
     ()
 
-    (*
-    let file = new System.IO.StreamWriter(@"C:\Users\User\Documents\Workspace\test.txt")
-
-    code.ForEach( 
-        fun x -> 
-        ( 
-            //printf "("
-            Array.iter( fun y -> file.Write(Asm.toString(y, TTA)); file.Write("; ")) x
-            file.WriteLine()
-            //printfn ")"
-        ) 
-    )
-
-    file.Close()
-    *)
+    //printCodeInFile(TTA, code, @"C:\Users\User\Documents\Workspace\test.txt")
 
 [<Test>]
 let SimpleCompilingFromFSharpToAsm2() = 
@@ -141,35 +76,27 @@ let SimpleCompilingFromFSharpToAsm2() =
     let FU2 = REGISTER("0", true)
     let TTA = new TTA([| (FU1, 5); (FU2, 4) |], 2)
 
+    TTA.SetFUAsNonFree(inits.[0].ResultAddr)
+    TTA.SetFUAsNonFree(inits.[1].ResultAddr)
+    TTA.SetFUAsNonFree(inits.[2].ResultAddr)
+    TTA.SetFUAsNonFree(terminals.[0].ResultAddr)
+
     let compiler = new VSFGCompiler(vsfg, TTA)
 
     let code = compiler.Compile()
 
     ()
-    (*
-    let file = new System.IO.StreamWriter(@"C:\Users\User\Documents\Workspace\test.txt")
+    //printCodeInFile(TTA, code, @"C:\Users\User\Documents\Workspace\test.txt")
 
-    code.ForEach( 
-        fun x -> 
-        ( 
-            //printf "("
-            Array.iter( fun y -> file.Write(Asm.toString(y, TTA)); file.Write("; ")) x
-            file.WriteLine()
-            //printfn ")"
-        ) 
-    )
 
-    file.Close()
-    *)
-
-(*[<Test>]
+[<Test>]
 let MoreComplexCompilingFromFSharpToAsm() =     
     let t = VSFGConstructor("
 
     let main (x:int) (y:int) (z : int) :int = (x + y) + z + x + x + y + z + x + y + z / x + y / z + (x + y) + z + x + x + y + z + x + y + z / x + y / z + (x + y) + z + x + x + y + z + x + y + z / x + y / z + (x + y) + z + x + x + y + z + x + y + z / x + y / z
     "
         )
-    let vsfg = t.getVSFG (t.Helper.getFSharpExpr 0)
+    let vsfg = t.getVSFG
 
     let inits = vsfg.InitialNodes
     inits.[0].ResultAddr <- (1<ln>, 0<col>)
@@ -185,24 +112,97 @@ let MoreComplexCompilingFromFSharpToAsm() =
     let FU4 = DIV("in1", "in2t", "out1", true)
     let TTA = new TTA([| (FU1, 5); (FU2, 5); (FU3, 5); (FU4, 5) |], 3)
 
+    TTA.SetFUAsNonFree(inits.[0].ResultAddr)
+    TTA.SetFUAsNonFree(inits.[1].ResultAddr)
+    TTA.SetFUAsNonFree(inits.[2].ResultAddr)
+    TTA.SetFUAsNonFree(terminals.[0].ResultAddr)
+
     let compiler = new VSFGCompiler(vsfg, TTA)
 
     let code = compiler.Compile()
-    //ай-я-яй=)
-    //конечно не буду это пушить в репозиторий, это же только у меня запустится!!
 
-    let file = new System.IO.StreamWriter(@"C:\Users\User\Documents\Workspace\test.txt")
+    ()
+    //printCodeInFile(TTA, code, @"C:\Users\User\Documents\Workspace\test.txt")
 
-    code.ForEach( 
-        fun x -> 
-        ( 
-            //printf "("
-            Array.iter( fun y -> file.Write(Asm.toString(y, TTA)); file.Write("; ")) x
-            file.WriteLine()
-            //printfn ")"
-        ) 
-    )
+[<Test>]
+let SimpleIfConstructions () = 
+    let t = VSFGConstructor("
 
-    file.Close()
+    let main (x:int) (y:int) (z : int) :int = if x < y
+                                              then
+                                                x + y
+                                              else
+                                                x + z
+                                              
+    "
+        )
+    let vsfg = t.getVSFG
 
-    *)
+    let inits = vsfg.InitialNodes
+    inits.[0].ResultAddr <- (1<ln>, 0<col>)
+    inits.[1].ResultAddr <- (1<ln>, 1<col>)
+    inits.[2].ResultAddr <- (1<ln>, 2<col>)
+
+    let terminals = vsfg.TerminalNodes
+    terminals.[0].ResultAddr <- (1<ln>, 3<col>)
+
+    let FU1 = ADD("in1", "in2t", "out1", true)
+    let FU2 = REGISTER("0", true)
+    let FU3 = BOOL("0", true)
+    let FU4 = LT("in1", "in2t", "out1", true)
+    let FU5 = DIV("in1", "in2t", "out1", true)
+    let FU6 = PC("000", true)
+    let TTA = new TTA([| (FU1, 2); (FU2, 10); (FU3, 5); (FU4, 5); (FU5, 1); (FU6, 1) |], 3)
+    
+    TTA.SetFUAsNonFree(inits.[0].ResultAddr)
+    TTA.SetFUAsNonFree(inits.[1].ResultAddr)
+    TTA.SetFUAsNonFree(inits.[2].ResultAddr)
+    TTA.SetFUAsNonFree(terminals.[0].ResultAddr)
+    
+    let compiler = new VSFGCompiler(vsfg, TTA)
+
+    let code = compiler.Compile()
+
+    ()
+    //printCodeInFile(TTA, code, @"C:\Users\User\Documents\Workspace\test.txt")
+
+[<Test>]
+let MoreComplexIfConstructions () = 
+    let t = VSFGConstructor("
+
+    let main (x:int) (y:int) (z : int) :int = if x < y
+                                              then
+                                                x
+                                              else
+                                                z
+    "
+        )
+    let vsfg = t.getVSFG
+
+    let inits = vsfg.InitialNodes
+    inits.[0].ResultAddr <- (1<ln>, 0<col>)
+    inits.[1].ResultAddr <- (1<ln>, 1<col>)
+    inits.[2].ResultAddr <- (1<ln>, 2<col>)
+
+    let terminals = vsfg.TerminalNodes
+    terminals.[0].ResultAddr <- (1<ln>, 3<col>)
+
+    let FU1 = ADD("in1", "in2t", "out1", true)
+    let FU2 = REGISTER("0", true)
+    let FU3 = BOOL("0", true)
+    let FU4 = LT("in1", "in2t", "out1", true)
+    let FU5 = DIV("in1", "in2t", "out1", true)
+    let FU6 = PC("000", true)
+    let TTA = new TTA([| (FU1, 7); (FU2, 10); (FU3, 5); (FU4, 5); (FU5, 3); (FU6, 1) |], 3)
+    
+    TTA.SetFUAsNonFree(inits.[0].ResultAddr)
+    TTA.SetFUAsNonFree(inits.[1].ResultAddr)
+    TTA.SetFUAsNonFree(inits.[2].ResultAddr)
+    TTA.SetFUAsNonFree(terminals.[0].ResultAddr)
+    
+    let compiler = new VSFGCompiler(vsfg, TTA)
+
+    let code = compiler.Compile()
+
+    ()
+    //printCodeInFile(TTA, code, @"C:\Users\User\Documents\Workspace\test.txt")
