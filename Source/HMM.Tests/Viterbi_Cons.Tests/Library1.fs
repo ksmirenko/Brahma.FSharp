45727c832b523c75650642b3ce945945afb23953
KochetovKirill <kirya.god@yandex.ru>
2016-04-19 17:18:42 +0300

    consistent version done, first test added

---
+++ Source/HMM.Tests/Viterbi_Cons.Tests/Library1.fs
@@ -0,0 +1,23 @@
+﻿module Viterbi_Cons.Tests
+
+open Viterbi_Cons
+open NUnit.Framework
+
+
+[<Test>]
+let ``first`` () =
+   let observSpace = [|"normal"; "cold"; "dizzy"|]
+   let stateSpace = [|"Healthy"; "Fever"|]
+   let startProbs = [|0.6; 0.4|]
+   let transitionProbs = [| [|0.7; 0.3|]; [|0.4; 0.6|] |]
+   let emissionProbs = [| [|0.5; 0.4; 0.1|]; [|0.1; 0.3; 0.6|] |]
+   let observeSeq = [|"normal"; "cold"; "dizzy"|]
+   let stateSeq = [|"Healthy"; "Healthy"; "Fever"|]
+   let res = viterbi 
+             <| [|0..observSpace.Length - 1|]
+             <| [|0..stateSpace.Length - 1|] 
+             <| startProbs 
+             <| [|for i in observeSeq -> Array.findIndex (fun x -> x = i) observSpace|] 
+             <| transitionProbs 
+             <| emissionProbs
+   Assert.AreEqual(stateSeq, [|for i in res -> stateSpace.[i]|])
---
+++ Source/Viterbi_Cons/Program.fs
@@ -0,0 +1,24 @@
+﻿module Viterbi_Cons
+
+let viterbi (observSpace: int[]) (stateSpace: int[]) (startProbs : double[])  (observSeq : int[]) (transitionProbs : double[][]) (emissionProbs : double[][]) =
+    let mutable X = [|0..observSeq.Length - 1|]
+    let mutable Z = [|0..observSeq.Length - 1|]
+
+    let T1 = [|for i in stateSpace -> 
+                            [|for j in 0..observSeq.Length - 1 ->
+                                    if j = 0
+                                    then startProbs.[i] * emissionProbs.[i].[observSeq.[0]]
+                                    else 0.0|]|]
+    let T2 = [|for i in stateSpace -> 
+                            [|for j in 0..observSeq.Length - 1 -> 0|]|]
+    for i in 1..observSeq.Length - 1 do
+        for j in stateSpace do
+            T1.[j].[i] <- Array.max([|for k in stateSpace -> 
+                                (T1.[k].[i - 1] * transitionProbs.[k].[j] * emissionProbs.[j].[observSeq.[i]])|])
+            T2.[j].[i] <- Array.maxBy(fun k -> T1.[k].[i - 1] * transitionProbs.[k].[j] * emissionProbs.[j].[observSeq.[i]]) stateSpace
+    Z.[observSeq.Length - 1] <- Array.maxBy(fun k -> T1.[k].[observSeq.Length - 1]) stateSpace
+    X.[observSeq.Length - 1] <- stateSpace.[Z.[observSeq.Length - 1]]
+    for i in 1..(observSeq.Length - 1) do
+        Z.[observSeq.Length - i - 1] <- T2.[Z.[observSeq.Length - i]].[observSeq.Length - i]
+        X.[observSeq.Length - i - 1] <- stateSpace.[Z.[observSeq.Length - i - 1]]
+    X
