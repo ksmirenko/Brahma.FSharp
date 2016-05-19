e080d8936890e6783a619c8adc70611f5cc880eb
Polina <lunina_polina@mail.ru>
2016-05-19 20:48:39 +0300

    compile func deleted

--- Source/ArrayFunc/ArrayGPU.fs
+++ Source/ArrayFunc/ArrayGPU.fs
@@ -15,7 +15,7 @@ let init (inArr: array<_>) =
         with
         | ex -> failwith ex.Message
     
-    let getLocalWorkSize (inArr: array<_>) = //доделать округление для некруглых чисел, зафиксировать минимальный lws
+    let getLocalWorkSize (inArr: array<_>) = 
             let lws, ex = OpenCL.Net.Cl.GetDeviceInfo(provider.Devices |> Seq.head, OpenCL.Net.DeviceInfo.MaxWorkGroupSize)
             let maxWorkSize = int <| lws.CastTo<uint64>()
             if inArr.Length <= maxWorkSize then inArr.Length
@@ -25,18 +25,11 @@ let init (inArr: array<_>) =
                     l <- l - 1
                 l
     let length = inArr.Length
+    if length = 0 then failwith "Array is empty"
     let localWorkSize = getLocalWorkSize inArr
     let mutable commandQueue = new CommandQueue(provider, provider.Devices |> Seq.head)
     let t = provider, commandQueue, length, localWorkSize
     t    
-
-let compile (provider: ComputeProvider) (commandQueue: CommandQueue) length localWorkSize command inArr =
-    let outArr = Array.zeroCreate length  
-    let kernel, kernelPrepare, kernelRun = provider.Compile command 
-    let d = new _1D(length, localWorkSize)
-    kernelPrepare d inArr outArr 
-    let _ = commandQueue.Add(kernelRun()) 
-    outArr 
 
 let Map func (inArr: array<_>) ((provider: ComputeProvider), (commandQueue: CommandQueue), length, localWorkSize) =
     let command = 
@@ -45,7 +38,11 @@ let Map func (inArr: array<_>) ((provider: ComputeProvider), (commandQueue: Comm
                 let r = rng.GlobalID0 
                 b.[r] <- (%func) a.[r]                                 
          @>
-    let outArr = compile (provider: ComputeProvider) (commandQueue: CommandQueue) length localWorkSize command inArr
+    let outArr = Array.zeroCreate length  
+    let kernel, kernelPrepare, kernelRun = provider.Compile command 
+    let d = new _1D(length, localWorkSize)
+    kernelPrepare d inArr outArr 
+    let _ = commandQueue.Add(kernelRun()) 
     outArr
 
 let Mapi func (inArr: array<_>) ((provider: ComputeProvider), (commandQueue: CommandQueue), length, localWorkSize) =  
@@ -56,35 +53,39 @@ let Mapi func (inArr: array<_>) ((provider: ComputeProvider), (commandQueue: Com
                 let i = r
                 b.[r] <- (%func) i a.[i]                               
         @>
-    let outArr = compile (provider: ComputeProvider) (commandQueue: CommandQueue) length localWorkSize command inArr
+    let outArr = Array.zeroCreate length  
+    let kernel, kernelPrepare, kernelRun = provider.Compile command 
+    let d = new _1D(length, localWorkSize)
+    kernelPrepare d inArr outArr 
+    let _ = commandQueue.Add(kernelRun()) 
     outArr
 
 let Map2 func (inArr1: array<_>) (inArr2: array<_>) ((provider: ComputeProvider), (commandQueue: CommandQueue), length, localWorkSize) = 
     if inArr1.Length <> inArr2.Length then failwith "Arrays must have the same lengths"
-    let outArr = Array.zeroCreate length 
     let command = 
         <@
             fun (rng: _1D) (a1: array<_>) (a2: array<_>) (b: array<_>) ->
                 let r = rng.GlobalID0 
                 b.[r] <- (%func) a1.[r] a2.[r]                               
         @>
-    let kernel, kernelPrepare, kernelRun = provider.Compile command
+    let outArr = Array.zeroCreate length  
+    let kernel, kernelPrepare, kernelRun = provider.Compile command 
     let d = new _1D(length, localWorkSize)
-    kernelPrepare d inArr1 inArr2 outArr
-    let _ = commandQueue.Add(kernelRun()).Finish()
+    kernelPrepare d inArr1 inArr2 outArr 
+    let _ = commandQueue.Add(kernelRun()) 
     outArr
 
 let Reverse (inArr: array<_>) ((provider: ComputeProvider), (commandQueue: CommandQueue), length, localWorkSize) = 
     let outArr = Array.zeroCreate length   
     let command =
         <@
-            fun (rng: _1D) (a: array<_>) (b: array<_>) length ->
+            fun (rng: _1D) (a: array<_>) (b: array<_>) length  ->
                 let r = rng.GlobalID0 
                 b.[length - r - 1] <- a.[r]                               
         @>
     let kernel, kernelPrepare, kernelRun = provider.Compile command
-    let d = new _1D(length,localWorkSize)    
-    kernelPrepare d inArr outArr length
+    let d = new _1D(length, localWorkSize)    
+    kernelPrepare d inArr outArr length 
     let _ = commandQueue.Add(kernelRun()).Finish()
     outArr
 
