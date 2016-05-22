233e8342e20c297791b9be72c144423babc79708
Polina <lunina_polina@mail.ru>
2016-05-22 23:37:53 +0300

    time tests (graphics and diagram)

--- Source/ArrayFun.TimeTests/ArrayGPU.TimeTests.fs
+++ Source/ArrayFun.TimeTests/ArrayGPU.TimeTests.fs
@@ -1,35 +1,53 @@
 ﻿module ArrayGPU.TimeTests
+open FSharp.Charting
+open System.Drawing
+open System.Windows.Forms
 
-let compare1 arr arrFunc parFunc myFunc n (name: string) = //functions that take 1 array. n=3 if parallel fun exist, n=2 otherwise
-    let t = ArrayGPU.init arr
-    let time f =
-        let beg = System.DateTime.Now
-        for i in 1..10 do f() |> ignore 
-        (((System.DateTime.Now - beg).TotalMilliseconds))/10.00
-    
-    System.Console.WriteLine (name)
-    System.Console.WriteLine ("Array: {0}", time (fun() -> arrFunc arr))
-    if n = 3 then System.Console.WriteLine ("ArrayParallel: {0}", time (fun() -> parFunc arr))
-    System.Console.WriteLine ("ArrayGPU: {0}", time (fun() -> myFunc arr t))
-
-let compare2 arr1 arr2 arrFunc parFunc myFunc n (name: string) = //functions that take 2 arrays - map2 only
+let diagram f i = 
+    let arr1 = [|for j in 1..i -> j|]
+    let time1 = System.DateTime.Now
     let t = ArrayGPU.init arr1
-    let time f =
-        let beg = System.DateTime.Now
-        for i in 1..10 do f() |> ignore 
-        ((System.DateTime.Now - beg).TotalMilliseconds)/10.00
-    
-    System.Console.WriteLine (name)
-    System.Console.WriteLine ("Array: {0}", time (fun() -> arrFunc arr1 arr2 ))
-    if n = 3 then System.Console.WriteLine ("ArrayParallel: {0}", time (fun() -> parFunc arr1 arr2 ))
-    System.Console.WriteLine ("ArrayGPU: {0}", time (fun() -> myFunc arr1 arr2 t))
+    let time2 = System.DateTime.Now
+    let outarr = f arr1 t
+    let time3 = System.DateTime.Now
+    let outarr = ArrayGPU.getResult outarr t
+    let time4 = System.DateTime.Now
+    let d = Chart.Pie ["initialization", (time2 - time1).TotalMilliseconds; "function", (time3 - time2).TotalMilliseconds; "getting result", (time4 - time3).TotalMilliseconds]
+    do System.Windows.Forms.Application.Run (d.ShowChart())
 
-let arr1 = [|for i in 1..10000000 -> i|]
-let arr2 = [|for i in 1..10000000 -> i|]
+diagram (ArrayGPU.Map <@ fun x -> x + 1 @>) 10000000 |> ignore
 
-compare1 arr1 (Array.map (fun x -> x + 2)) (Array.Parallel.map (fun x -> x + 2)) (ArrayGPU.Map (<@fun x -> x + 2 @>) ) 3 "MAP"
-compare1 arr1 (Array.mapi (fun i x -> x + i)) (Array.Parallel.mapi (fun i x -> x + i)) (ArrayGPU.Mapi (<@ fun i x-> x + i @>) ) 3 "\nMAPI"
-compare2 arr1 arr2 (Array.map2 (fun i x -> x + i)) (Array.map2 (fun i x -> x + i)) (ArrayGPU.Map2 (<@ fun i x-> x + i @>) ) 2 "\nMAP2"
-compare1 arr1 (Array.rev ) (Array.rev) (ArrayGPU.Reverse) 2 "\nREVERSE"
 
-let n = System.Console.ReadLine()
+let timer1 f i = //for Array and Array.Parallel
+    if i = 0 then 0.00
+    else
+        let arr1 = [|for j in 1..i -> j|]
+        //let arr2 = [|for j in 1..i -> j|]
+        let time f =
+            let beg = System.DateTime.Now
+            for i in 1..3 do f() |> ignore 
+            ((System.DateTime.Now - beg).TotalMilliseconds) / 3.00
+        let res = (time (fun() -> f arr1 (*arr2*)))
+        res
+
+let timer2 f i = //for ArrayGPU
+    if i = 0 then 0.00
+    else
+        let arr1 = [|for j in 1..i -> j|]
+        //let arr2 = [|for j in 1..i -> j|]
+        let t = ArrayGPU.init arr1 
+        let time f =
+            let beg = System.DateTime.Now
+            for i in 1..3 do f() |> ignore 
+            ((System.DateTime.Now - beg).TotalMilliseconds)/3.00
+        let res = (time (fun() -> f arr1 (*arr2*)  t))
+        ArrayGPU.getResult (f arr1 (*arr2*) t) t |>ignore
+        res
+
+let main = 
+    Chart.Combine (
+        [Chart.Line ([for i in 1000000..500000..10000000 -> (i, timer1 (Array.map (fun x -> sin(float(x)))) i)], Name = "Array", Color = System.Drawing.Color.Green)
+         Chart.Line ([for i in 1000000..500000..10000000 -> (i, timer1 (Array.Parallel.map (fun x -> sin(float(x)))) i)], Name = "Parallel", Color = System.Drawing.Color.Blue)
+         Chart.Line ([for i in 1000000..500000..10000000 -> (i, timer2 (ArrayGPU.Map <@ fun x -> sin(float(x)) @>) i)], Name = "GPU", Color = System.Drawing.Color.Red)]
+         )
+do System.Windows.Forms.Application.Run (main.ShowChart())
