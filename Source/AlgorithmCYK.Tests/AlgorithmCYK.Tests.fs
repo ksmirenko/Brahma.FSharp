0adc6354f8d3f6ce54dbeabc064e0268bc2a8b45
SusaninaJulia <u.a.susxv@mail.ru>
2016-05-25 14:05:16 +0300

    CYK draft

--- Source/AlgorithmCYK.Tests/AlgorithmCYK.Tests.fs
+++ Source/AlgorithmCYK.Tests/AlgorithmCYK.Tests.fs
@@ -4,36 +4,30 @@ open NUnit.Framework
 
 [<Test>]
 let ``CYK returns "The string can be generated. " (grammar 1)`` () =
-  let result = CYK [|("S","AB"); ("S","BC"); ("A","BA"); ("B","CC"); ("C","AB"); ("B","b"); ("A","a"); ("C","a")|] "baaba" "S"
-  printfn "%s" result
-  Assert.AreEqual("The string can be generated. ", result)
+  let result = CYK [("S","AB"); ("S","BC"); ("A","BA"); ("B","CC"); ("C","AB"); ("B","b"); ("A","a"); ("C","a")] [|"S";"A";"B";"C"|] "baaba" "S"
+  Assert.AreEqual(true, result)
 
 [<Test>]
 let ``CYK returns "The string cannot be generated. " (grammar 1)`` () =
-  let result = CYK [|("S","AB"); ("S","BC"); ("A","BA"); ("B","CC"); ("C","AB"); ("B","b"); ("A","a"); ("C","a")|] "baaababbbabab" "S"
-  printfn "%s" result
-  Assert.AreEqual("The string cannot be generated. ", result)
+  let result = CYK [("S","AB"); ("S","BC"); ("A","BA"); ("B","CC"); ("C","AB"); ("B","b"); ("A","a"); ("C","a")] [|"S";"A";"B";"C"|] "baaababbbabab" "S"
+  Assert.AreEqual(false, result)
 
 [<Test>]
 let ``CYK returns "The string's lenght < 1. " (grammar 1)`` () =
-  let result = CYK [|("S","AB"); ("S","BC"); ("A","BA"); ("B","CC"); ("C","AB"); ("B","b"); ("A","a"); ("C","a")|] "" "S"
-  printfn "%s" result
-  Assert.AreEqual("The string's lenght < 1. ", result)
+  let result = CYK [("S","AB"); ("S","BC"); ("A","BA"); ("B","CC"); ("C","AB"); ("B","b"); ("A","a"); ("C","a")] [|"S";"A";"B";"C"|] "" "S"
+  Assert.AreEqual(true, result)
 
 [<Test>]
 let ``CYK returns "The string can be generated. " (grammar 2)`` () =
-  let result = CYK [|("S","AB"); ("A","CC"); ("B","BC"); ("C","CB"); ("C","BA"); ("B","b"); ("A","a"); ("A","c"); ("C","c")|] "bccbcabcb" "S"
-  printfn "%s" result
-  Assert.AreEqual("The string can be generated. ", result)
+  let result = CYK [("S","AB"); ("A","CC"); ("B","BC"); ("C","CB"); ("C","BA"); ("B","b"); ("A","a"); ("A","c"); ("C","c")] [|"S";"A";"B";"C"|] "bccbcabcb" "S"
+  Assert.AreEqual(true, result)
 
 [<Test>]
 let ``CYK returns "The string cannot be generated. " (grammar 2)`` () =
-  let result = CYK [|("S","AB"); ("A","CC"); ("B","BC"); ("C","CB"); ("C","BA"); ("B","b"); ("A","a"); ("A","c"); ("C","c")|] "cccccaaaabbb" "S"
-  printfn "%s" result
-  Assert.AreEqual("The string cannot be generated. ", result)
+  let result = CYK [("S","AB"); ("A","CC"); ("B","BC"); ("C","CB"); ("C","BA"); ("B","b"); ("A","a"); ("A","c"); ("C","c")] [|"S";"A";"B";"C"|] "cccccaaaabbb" "S"
+  Assert.AreEqual(false, result)
 
 [<Test>]
 let ``CYK returns "The string's lenght < 1. " (grammar 2)`` () =
-  let result = CYK [|("S","AB"); ("A","CC"); ("B","BC"); ("C","CB"); ("C","BA"); ("B","b"); ("A","a"); ("A","c"); ("C","c")|] "" "S"
-  printfn "%s" result
-  Assert.AreEqual("The string's lenght < 1. ", result)
+  let result = CYK [("S","AB"); ("A","CC"); ("B","BC"); ("C","CB"); ("C","BA"); ("B","b"); ("A","a"); ("A","c"); ("C","c")] [|"S";"A";"B";"C"|] "" "S"
+  Assert.AreEqual(true, result)
--- Source/AlgorithmCYK/AlgorithmCYK.fs
+++ Source/AlgorithmCYK/AlgorithmCYK.fs
@@ -2,103 +2,89 @@
 open System
 
 let rulesInput() = 
-    let array = [||]
-    let rec rulesInput array =
+    printfn "%s" "Enter a context-free grammar in Chomsky normal form: \n"
+    let rec rulesInput list (set : Set<string>) =
         printfn "%s" "Enter a left part of the rule:  "
         let lp = Console.ReadLine()
+        set.Add(lp) |> ignore
         printfn "%s" "Enter a right part of the rule:  "
         let rp = Console.ReadLine()
         printfn "%s" "Press ENTER to continue or SPACE to end."
         let rk = Console.ReadKey()
         if (rk.Key).ToString() = "Spacebar"
-        then Array.append array [|(lp,rp)|]
-        else rulesInput (Array.append array [|(lp,rp)|])
-    rulesInput array
+        then (lp,rp) :: list, set
+        else rulesInput ((lp,rp) :: list) set
+    rulesInput [] Set.empty   
+        
+let allRulesCheck (rules : list<string * string>) (nonterm : string array) (ntgn : int array) (comp : string array) = 
+    for i = 0 to comp.Length - 1 do
+        for j = 0 to rules.Length - 1 do
+            match rules.[j] with 
+            | (ch, str) -> if comp.[i] = str
+                           then
+                               for k = 0 to nonterm.Length - 1 do
+                                   if nonterm.[k] = ch
+                                   then ntgn.[k] <- 1
 
-let oneRuleCheck ((ch : string),(str: string)) (comp : string) =
-    if comp = str 
-    then [|ch|]
-    else [||]
 
-let allRulesCheck (rules : array<string * string>) (comp : string array) = 
-    let len = comp.Length
-    let arr = ResizeArray<string>()
-    let n = rules.Length
-    for j in 0..len - 1 do
-        for i in 0..n - 1 do
-           arr.AddRange(oneRuleCheck rules.[i] comp.[j])
-    arr.ToArray()
-
-let deleteEqual (arr : array<'t>) = 
-    match arr with 
-    | [||] -> arr
-    | _ ->   
-        let n = arr.Length
-        let res = ResizeArray<'t>()
-        res.Add(arr.[0])
-        for i = 1 to n - 1 do
-            let mutable f = 0
-            let reslen = res.Count
-            for j = 0 to reslen - 1 do
-                if arr.[i] = res.[j]
-                then f <- 1
-            if f = 0
-            then 
-                res.Add(arr.[i])
-        res.ToArray()
-
-let decMultiply (arr1 : array<string>) (arr2 : array<string>) =
-    let res = ResizeArray<string>()
-    let n1 = arr1.Length
-    let n2 = arr2.Length
-    match n1, n2 with
-    | 0, n2 -> arr2
-    | n1, 0 -> arr1
-    | _, _ -> for i in 0..n1 - 1 do
-                  for j in 0..n2 - 1 do
-                      res.Add(arr1.[i] + arr2.[j])
-              res.ToArray()
-
-let matrixCYK (rules : array<string * string>) (str: string) (n: int) (matrix  : string array [][]) = 
-    let dm = ResizeArray<string>() 
-    for i = 0 to n - 1 do 
-        matrix.[n - 1].[i] <- allRulesCheck rules [|(str.[i].ToString())|]
+let compCreate (nonterm : string array) (ntgen1 : int array) (ntgen2 : int array) =
+    let comp = ResizeArray<string>() 
+    for i = 0 to ntgen1.Length - 1 do
+        if ntgen1.[i] = 1
+        then
+            for j = 0 to ntgen2.Length - 1 do
+                if ntgen2.[i] = 1
+                then comp.Add(nonterm.[i] + nonterm.[j])
+    comp.ToArray()
+              
+let matrixCYK (rules : list<string * string>) (nonterm : string array) (str : string) (n : int) (matrix : int array [][]) = 
+    for k = 0 to n - 1 do 
+        allRulesCheck rules nonterm matrix.[n - 1].[k] [|(str.[k].ToString())|]
+    printfn "%A" matrix
     for l = 1 to n - 1 do
         for i = 0 to n - l - 1 do
             for j = 0 to l - 1 do
-                dm.AddRange(decMultiply matrix.[n - 1 - j].[i] matrix.[j + n - l].[i + j + 1])
-                matrix.[n - 1 - l].[i]  <- deleteEqual (Array.append matrix.[n - 1 - l].[i] (allRulesCheck rules (dm.ToArray())))
-                dm.Clear()
+                let x = compCreate nonterm matrix.[n - 1 - j].[i] matrix.[j + n - l].[i + j + 1]
+                allRulesCheck rules nonterm matrix.[n - 1 - l].[i] x
     matrix
- 
-let conclCYK (matrix : string array [][]) (start : string) = 
-    let rec concCYK (arr : string array) (i : int) = 
-        if i <> Array.length(arr) 
-        then
-            if arr.[i] = start 
-            then "The string can be generated. "
-            else concCYK arr (i + 1)
-        else "The string cannot be generated. "
-    concCYK matrix.[0].[0] 0
 
-let CYK (rules : (string*string) []) (str : string) (start : string) = 
-    let n = str.Length
-    if n <> 0 
-    then
-        let matrix  : string array [][] =  [| for i in 0 .. n - 1 do yield [| for j in 0 .. n - 1 do yield [|""|] |] |]
-        let matrCYK = matrixCYK rules str n matrix
-        conclCYK matrCYK start
-    else "The string's lenght < 1. "
- 
+let conclCYK (matrix : int array [][]) (nonterm : string array) (start : string) = 
+    let mutable ind = -1
+    for i = 0 to nonterm.Length - 1 do
+        if nonterm.[i] = start
+        then ind <- i
+    if matrix.[0].[0].[ind] = 1
+    then true
+    else false
+
+let CYK (rules : list<string*string>) (nonterm : string array) (str : string) (start : string) = 
+    if str = ""
+    then true
+    else 
+        let n = str.Length 
+        let ntgen = Array.zeroCreate (nonterm.Length)
+        let matrix  : int array [][] =  [|for i in 0..n - 1 -> [|for j in 0..n - 1 -> ntgen|] |]
+        printfn "%A" matrix
+        let matrCYK = matrixCYK rules nonterm str n matrix
+        conclCYK matrCYK nonterm start
+        
+let result = CYK [("S","AB"); ("S","BC"); ("A","BA"); ("B","CC"); ("C","AB"); ("B","b"); ("A","a"); ("C","a")] [|"S";"A";"B";"C"|] "baaba" "S"
+printfn "%b" result
+
 let mainCYK() = 
     printfn "%s" "Enter a context-free grammar in Chomsky normal form ('S' is a start symbol): \n"
     printfn "%s" "Enter a start symbol of the grammar:  "
     let s = Console.ReadLine()
-    let rules = rulesInput()
+    let (rul, nonterm) = rulesInput()
     printfn "%s" "Enter a string for the CYK algorithm: \n"
     let str = Console.ReadLine()
-    printfn "%s" (CYK rules str s)
+    if str.Length = 0 
+    then printfn "%s" "The string's lenght < 1. "
+    elif (CYK rul (Set.toArray(nonterm)) str s)
+    then printfn "%s" "The string can be generated. "
+    else printfn "%s" "The string cannot be generated. "
     Console.ReadKey(true) |> ignore
 
-mainCYK()
+Console.ReadKey(true) |> ignore
+//mainCYK()
 
