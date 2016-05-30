3c445af04fc8d49ee382262a0536534820df08b2
SusaninaJulia <u.a.susxv@mail.ru>
2016-05-30 16:52:48 +0300

    fix

--- Source/AlgorithmCYK.ParallelCPU/AlgorithmCYK.ParallelCPU.fs
+++ Source/AlgorithmCYK.ParallelCPU/AlgorithmCYK.ParallelCPU.fs
@@ -3,7 +3,7 @@ open AlgorithmCYK
 open System.Threading.Tasks
 
 let matrixCYKParallelCPU (rules : list<string * string>) (nonterm : string array) (str : string) (n : int) (matrix : int array [][]) = 
-    Array.Parallel.iteri (fun i arr -> (allRulesCheck rules nonterm arr [|(str.[i].ToString())|])) matrix.[n - 1]
+    Array.Parallel.iteri (fun i arr -> allRulesCheck rules nonterm arr [|(str.[i].ToString())|]) matrix.[n - 1]
     for l = 1 to n - 1 do
         for i = 0 to n - l - 1 do
             Parallel.For(0, l, fun j -> allRulesCheck rules nonterm matrix.[n - 1 - l].[i] (compCreate nonterm matrix.[n - 1 - j].[i] matrix.[j + n - l].[i + j + 1]))
