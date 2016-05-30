3d7e97288c62b86b19c5af9dae2b7ca7e1cb4595
SusaninaJulia <u.a.susxv@mail.ru>
2016-05-30 12:17:27 +0300

    CYK parallel CPU

---
+++ Source/AlgorithmCYK.ParallelCPU.Tests/AlgorithmCYK.ParallelCPU.Tests.fs
@@ -0,0 +1,44 @@
+﻿module AlgorithmCYK.ParallelCPU.Tests
+open AlgorithmCYK
+open AlgorithmCYK.ParallelCPU
+open NUnit.Framework
+
+[<TestFixture>]
+
+type TestsGrammar1() = 
+    
+    let (rl1, non1, start1) = ([("S","AB"); ("S","BC"); ("A","BA"); ("B","CC"); ("C","AB"); ("B","b"); ("A","a"); ("C","a")], [|"S";"A";"B";"C"|], "S")
+
+    [<Test>]
+    member this.``CYKParallelCPU returns true`` () =
+      let result = CYKParallelCPU rl1 non1 "baaba" start1
+      Assert.AreEqual(true, result)
+
+    [<Test>]
+    member this.``CYKParallelCPU returns false`` () =
+      let result = CYKParallelCPU rl1 non1 "baaababbbabab" start1
+      Assert.AreEqual(false, result)
+
+    [<Test>]
+    member this.``CYKParallelCPU returns true (empty string)`` () =
+      let result = CYKParallelCPU rl1 non1 "" start1
+      Assert.AreEqual(true, result)
+
+type TestsGrammar2() = 
+
+    let (rl2, non2, start2) = ([("S","AB"); ("A","CC"); ("B","BC"); ("C","CB"); ("C","BA"); ("B","b"); ("A","a"); ("A","c"); ("C","c")], [|"S";"A";"B";"C"|],"S")
+
+    [<Test>]
+    member this.``CYKParallelCPU returns true`` () =
+      let result = CYKParallelCPU rl2 non2 "bccbcabcb" start2
+      Assert.AreEqual(true, result)
+
+    [<Test>]
+    member this.``CYKParallelCPU returns false`` () =
+      let result = CYKParallelCPU rl2 non2 "cccccaaaabbb" start2
+      Assert.AreEqual(false, result)
+
+    [<Test>]
+    member this.``CYKParallelCPU returns true (empty string)`` () =
+      let result = CYKParallelCPU rl2 non2 "" start2
+      Assert.AreEqual(true, result)
---
+++ Source/AlgorithmCYK.ParallelCPU/AlgorithmCYK.ParallelCPU.fs
@@ -0,0 +1,20 @@
+﻿module AlgorithmCYK.ParallelCPU
+open AlgorithmCYK
+open System.Threading.Tasks
+
+let matrixCYKParallelCPU (rules : list<string * string>) (nonterm : string array) (str : string) (n : int) (matrix : int array [][]) = 
+    Parallel.For(0, n, fun k -> allRulesCheck rules nonterm matrix.[n - 1].[k] [|(str.[k].ToString())|]) |> ignore
+    for l = 1 to n - 1 do
+        for i = 0 to n - l - 1 do
+            Parallel.For(0, l, fun j -> allRulesCheck rules nonterm matrix.[n - 1 - l].[i] (compCreate nonterm matrix.[n - 1 - j].[i] matrix.[j + n - l].[i + j + 1]))
+            |> ignore
+    matrix
+
+let CYKParallelCPU (rules : list<string * string>) (nonterm : string array) (str : string) (start : string) = 
+    if str = ""
+    then true
+    else 
+        let n = str.Length 
+        let matrix =  [|for i in 0..n - 1 -> [|for j in 0..n - 1 -> Array.zeroCreate nonterm.Length|]|]
+        let matrCYK = matrixCYKParallelCPU rules nonterm str n matrix
+        conclCYK matrCYK nonterm start
