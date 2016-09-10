96ae121d4090c2145e24ce7a1e85bb0c870f936e
SusaninaJulia <u.a.susxv@mail.ru>
2016-09-10 23:13:03 +0300

    form

--- Source/AlgorithmCYK.ParallelCPU/AlgorithmCYK.ParallelCPU.fs
+++ Source/AlgorithmCYK.ParallelCPU/AlgorithmCYK.ParallelCPU.fs
@@ -3,7 +3,7 @@ open AlgorithmCYK
 open System.Threading.Tasks
 
 let matrixCYKParallelCPU (begtRules : array<int>) (endtRules : array<int>) (begnRules : array<int>) (endnRules : array<int>*array<int>) (str : array<int>) n (matrix : array<array<array<int>>>) = 
-    Array.Parallel.iteri (fun i arr -> rulesCheck str.[i] begtRules endtRules arr) matrix.[n - 1]
+    Array.Parallel.iteri(fun i arr -> rulesCheck str.[i] begtRules endtRules arr) matrix.[n - 1]
     for l = 1 to n - 1 do
         for j = 0 to l - 1 do
             Array.Parallel.iteri(fun i arr -> compCheck begnRules endnRules matrix.[n - 1 - j].[i] matrix.[j + n - l].[i + j + 1] arr) matrix.[n - l - 1].[..n - l - 1]
