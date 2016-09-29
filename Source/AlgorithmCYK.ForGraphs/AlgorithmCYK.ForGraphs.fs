88678058d7617c108c4e585e498feda318f3d8a3
SusaninaJulia <u.a.susxv@mail.ru>
2016-09-29 23:05:04 +0300

    try gpu

---
+++ Source/AlgorithmCYK.ForGraphs/AlgorithmCYK.ForGraphs.fs
@@ -0,0 +1,68 @@
+﻿module AlgorithmCYK.ForGraphs
+open AlgorithmCYK
+
+let diagonalMatrixCYK (begtRules : array<int>) (endtRules : array<int>) (begnRules : array<int>) (endnRules : array<int>*array<int>) (str : array<int>) n (matrix : array<array<array<int>>>) = 
+    for k = 0 to n - 1 do 
+        rulesCheck str.[k] begtRules endtRules matrix.[n - 1].[k] 
+    for l = 1 to n - 1 do
+        for i = 0 to n - l - 1 do
+            for j = 0 to l - 1 do
+                if n - l - 1 = i 
+                then compCheck begnRules endnRules matrix.[n - 1 - j].[i] matrix.[j + n - l].[i + j + 1] matrix.[n - 1 - l].[i]
+    matrix
+
+let matrixShift (mtrx : array<array<array<int>>>) =
+    let len = mtrx.Length
+    let save = [|for i in 0..len - 1 -> [|for j in 0..len - 1 -> Array.zeroCreate (mtrx.[0].[0]).Length|]|]
+    for i = 0 to len - 1 do
+        for j = 1 to len - 1 do
+        save.[i].[j - 1] <- mtrx.[i].[j]
+    save 
+
+let searchMtrx (mtrx : array<array<array<int>>>) ind n k (str : string) =
+    let arr = ResizeArray<string>()
+    for i = n - 1 to k - 1 do
+        if mtrx.[i].[0].[ind] = 1
+        then 
+            arr.Add(str.[0..i + 1])
+    arr
+
+let strInRange (str : string) n k (term, nonterm, begnRules, endnRules, begtRules, endtRules) (start : string) = 
+    let arrOfStr = ResizeArray<string>()
+    let ind = findIndex nonterm start
+    let len = str.Length
+    let matrix =  [|for i in 0..k - 1 -> [|for j in 0..k - 1 -> Array.zeroCreate nonterm.Length|]|]
+    let mutable res = matrixCYK begtRules endtRules begnRules endnRules (formStr term str.[0..k - 1]) k matrix
+    arrOfStr.AddRange(searchMtrx res ind n k str.[0..k - 1])
+    let save = matrixShift res
+    for i = 1 to len - k - 1 do
+        res <- diagonalMatrixCYK begtRules endtRules begnRules endnRules (formStr term str.[i..i + k - 1]) k save
+        arrOfStr.AddRange(searchMtrx res ind n k str.[i..i + k - 1])
+        res <- matrixShift res
+    arrOfStr 
+
+let search (mtrx : array<array<string>>) n k (rules : array<string * string>) (nonterm : array<string>) (start : string) = 
+    let (term, nonterm, begnRules, endnRules, begtRules, endtRules) = formRules rules
+    let rl = (term, nonterm, begnRules, endnRules, begtRules, endtRules)
+    let visited = Array.zeroCreate (mtrx.[0].Length)
+    let arr = ref (ResizeArray<string>())
+    let rec search a b n k (inWork : string) = 
+        if a <> b 
+        then 
+            for i = 0 to b do
+                if mtrx.[a].[i] <> "" && visited.[i] = 0
+                then 
+                    visited.[i] <- 1
+                    let len = (inWork + mtrx.[a].[i]).Length
+                    if len < k
+                    then 
+                        search i b n k (inWork + mtrx.[a].[i]) 
+                    else 
+                        (!arr).AddRange(strInRange (inWork + mtrx.[a].[i]) n k rl start)
+                        search i b n k (inWork + mtrx.[a].[i]).[len - k..len - 1]
+                    visited.[i] <- 0
+        else 
+            if inWork.Length >= n
+            then (!arr).AddRange(strInRange inWork n inWork.Length rl start)
+    search 0 (mtrx.[0].Length - 1) n k ""
+    (!arr).ToArray()
--- Source/AlgorithmCYK.ParallelGPGPU/AlgorithmCYK.ParallelGPGPU.fs
+++ Source/AlgorithmCYK.ParallelGPGPU/AlgorithmCYK.ParallelGPGPU.fs
@@ -71,17 +71,17 @@ let compCheckParal (begnRules : array<int>) (endnRulesL : array<int>) (endnRules
         <@
             fun (rng : _1D) (begnR : array<_>) (endnRL : array<_>) (endnRR : array<_>) (mtrxel1 : array<_>) (mtrxel2 : array<_>) (mtrxel3 : array<_>) lenEl lenR a b cols (toArr : array<_>) ->
                 let r = rng.GlobalID0
-                let mtrx1Shift = r * cols
-                let mtrx2Shift = (r + b + 1) * cols
+                let cols1Shift = r * cols
+                let cols2Shift = (r + b + 1) * cols
                 for i = 0 to lenEl - 1 do
-                    if mtrxel1.[mtrx1Shift + i] = 1
+                    if mtrxel1.[cols1Shift + i] = 1
                     then 
                         for j = 0 to lenEl - 1 do
-                            if mtrxel2.[mtrx2Shift + j] = 1
+                            if mtrxel2.[cols2Shift + j] = 1
                             then
                                 for k = 0 to lenR - 1 do
                                     if (i = endnRL.[k]) && (j = endnRR.[k])
-                                    then toArr.[mtrx1Shift + begnR.[k]] <- 1     
+                                    then toArr.[cols1Shift + begnR.[k]] <- 1     
         @>
     let newMtrxel = toArr mtrxel3
     let length = newMtrxel.Length 
@@ -111,3 +111,96 @@ let CYKParallelGPGPU (rules : array<string*string>) str start =
         let matrix =  [|for i in 0..n - 1 -> [|for j in 0..n - 1 -> Array.zeroCreate nonterm.Length|]|]
         let matrCYK = matrixCYKParallelGPGPU begtRules endtRules begnRules endnRulesL endnRulesR strArr n matrix
         conclCYK matrCYK nonterm start
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let allCYKGPU (meh : array<array<array<_>>>) nontermNumb (begnRules : array<int>) (endnRules : array<int> * array<int>) =
+    let (endnRulesL, endnRulesR) = fst endnRules, snd endnRules
+    let n = rows meh
+    let arr = toArr (toArr meh)
+    let l = arr.Length
+    let (provider, commandQueue) = provAndQueue()
+    let command = 
+        <@ 
+            fun (rng : _1D) (meh : array<int>) n nontermNumb (begnRules : array<int>) (endnRulesL : array<int>) enLen (endnRulesR : array<int>) (newMtrx : array<int>) l ->
+                for a = 1 to n - 1 do
+                    for b = 0 to a - 1 do 
+                        let r = rng.GlobalID0                
+                        if n - a >= r
+                        then 
+                            for i = 0 to nontermNumb - 1 do
+                                if meh.[(n - 1 - b) * n * nontermNumb + r * nontermNumb + i] = 1
+                                then 
+                                    for j = 0 to nontermNumb - 1 do
+                                        if meh.[(b + n - a) * n * nontermNumb + (b + r + 1) * nontermNumb + j] = 1
+                                        then 
+                                            for k = 0 to enLen - 1 do
+                                                if (i = endnRulesL.[k]) && (j = endnRulesR.[k])
+                                                then meh.[(n - 1 - a) * n * nontermNumb + r * nontermNumb + begnRules.[k]] <- 1
+                for k = 0 to l - 1 do
+                    newMtrx.[k] <- meh.[k]
+        @>
+
+    let rng = new _1D(n * n * nontermNumb, nontermNumb * n)
+    let kernel, kernelPrepare, kernelRun = provider.Compile command
+    let newMtrx = Array.zeroCreate l
+    kernelPrepare rng arr n nontermNumb begnRules endnRulesL endnRulesR.Length endnRulesR newMtrx l
+    let _ = commandQueue.Add(kernelRun())
+    let _ = commandQueue.Add(newMtrx.ToHost provider).Finish()
+    commandQueue.Dispose()
+    provider.Dispose()
+    provider.CloseAllBuffers()
+    toMtrx newMtrx (n * n) nontermNumb
+
+(*
+let allCYKGPU (meh : array<array<array<_>>>) nontermNumb (begnRules : array<int>) (endnRules : array<int> * array<int>) =
+    let (endnRulesL, endnRulesR) = fst endnRules, snd endnRules
+    let rows = rows meh
+    let cols = cols meh
+    let n = rows
+    let arr = toArr (toArr meh)
+    let l = arr.Length
+    let (provider, commandQueue) = provAndQueue()
+    for a = 1 to n - 1 do
+        for b = 0 to a - 1 do
+            for c = 0 to n - 1 do
+                if n - a - 1 >= c
+                then 
+                    for i = 0 to nontermNumb - 1 do
+                        if arr.[(n - 1 - b) * n * nontermNumb + c * nontermNumb + i] = 1
+                        then 
+                            for j = 0 to nontermNumb - 1 do
+                                if arr.[(b + n - a) * n * nontermNumb + (b + c + 1) * nontermNumb + j] = 1
+                                then 
+                                    for k = 0 to endnRulesR.Length - 1 do
+                                        if (i = endnRulesL.[k]) && (j = endnRulesR.[k])
+                                        then arr.[(n - 1 - a) * n * nontermNumb + c * nontermNumb + begnRules.[k]] <- 1
+    let newMtrx = Array.zeroCreate l
+    for k = 0 to l - 1 do
+        newMtrx.[k] <- arr.[k]
+    toMtrx newMtrx (rows * cols) nontermNumb
+*)
+
+let allCYKParallelGPGPU (begtRules : array<int>) (endtRules : array<int>) (begnRules : array<int>) (endnRules : array<int> *array<int>) nontermNumb (str : array<int>) n (matrix : array<array<array<int>>>) = 
+    matrix.[n - 1] <- rulesCheckParal begtRules endtRules matrix.[n - 1] str
+    printfn "%A" (toMtrx (allCYKGPU matrix nontermNumb begnRules endnRules) n n )
+    toMtrx (allCYKGPU matrix nontermNumb begnRules endnRules) n n 
+
+let CYKallParallelGPGPU (rules : array<string*string>) str start = 
+    if str = ""
+    then true
+    else 
+        let (term, nonterm, begnRules, endnRules, begtRules, endtRules) = formRules rules
+        let strArr = formStr term str
+        let n = str.Length 
+        let nontermNumb = nonterm.Length
+        let matrix =  [|for i in 0..n - 1 -> [|for j in 0..n - 1 -> Array.zeroCreate nonterm.Length|]|]
+        let matrCYK = allCYKParallelGPGPU begtRules endtRules begnRules endnRules nontermNumb strArr n matrix
+        conclCYK matrCYK nonterm start
+
+let (rl1, start1) = ([|("S","AB"); ("S","BC"); ("A","BA"); ("B","CC"); ("C","AB"); ("B","b"); ("A","a"); ("C","a")|], "S")
+let res = CYKallParallelGPGPU rl1 "baaba" start1
+for i = 0 to 20 do
+    printfn "%b" (CYKallParallelGPGPU rl1 "baaba" start1)
+
+System.Console.ReadKey(true) |> ignore
